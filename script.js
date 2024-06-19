5fedcfaea3a05409756a6b4aac3fe05d2226231b
Edson Emanoel <edson.emanoelofc@gmail.com>
2024-06-19 13:53:00 -0300

    O Projeto está Pronto

--- script.js
+++ script.js
@@ -1,53 +1,71 @@
-// Help
-function log(message){ 
-    console.log("> " + message) 
+/** help */
+function log(message) {
+    console.log('> ' + message)
 }
 
-// App
+/** app */
 const cards = document.querySelectorAll('.card')
 const dropzones = document.querySelectorAll('.dropzone')
 
 
-// cards
+/** our cards */
 cards.forEach(card => {
-    card.addEventListener("dragstart", dragstart);
-    card.addEventListener("drag", drag);
-    card.addEventListener("dragend", dragend);
+    card.addEventListener('dragstart', dragstart)
+    card.addEventListener('drag', drag)
+    card.addEventListener('dragend', dragend)
 })
 
+function dragstart() {
+    // log('CARD: Start dragging ')
+    dropzones.forEach( dropzone => dropzone.classList.add('highlight'))
 
-function dragstart(){
-    log("> CARD: Start dragging")
+    // this = card
+    this.classList.add('is-dragging')
 }
 
-function drag(){
-    log("> CARD: Is dragging")
+function drag() {
+    // log('CARD: Is dragging ')
 }
 
-function dragend(){
-    log("> CARD: Stop drag")
+function dragend() {
+    // log('CARD: Stop drag! ')
+    dropzones.forEach( dropzone => dropzone.classList.remove('highlight'))
+
+    // this = card
+    this.classList.remove('is-dragging')
 }
 
-// Local de onde soltar os cards
-// dropzones.forEach(dropzone => {
-//     dropzone.addEventListener('dragenter,', dragenter)
-//     dropzone.addEventListener('dragover', dragover)
-//     dropzone.addEventListener('dragleave', dragleave)
-//     dropzone.addEventListener('drop', drop)
-// })
+/** place where we will drop cards */
+dropzones.forEach( dropzone => {
+    dropzone.addEventListener('dragenter', dragenter)
+    dropzone.addEventListener('dragover', dragover)
+    dropzone.addEventListener('dragleave', dragleave)
+    dropzone.addEventListener('drop', drop)
+})
 
-// function dragenter (){
-//     log("> Dropzone: Enter in zone")
-// }
+function dragenter() {
+    // log('DROPZONE: Enter in zone ')
+}
 
-// function dragover (){
-//     log("> Dropzone: Over the zone")
-// }
+function dragover() {
+    // this = dropzone
+    this.classList.add('over')
 
-// function dragleave (){
-//     log("> Dropzone: Leave")
-// }
+    // get dragging card
+    const cardBeingDragged = document.querySelector('.is-dragging')
 
-// function drop (){
-//     log("> Dropzone: Dropped")
-// }
+    // this = dropzone
+    this.appendChild(cardBeingDragged)
+}
+
+function dragleave() {
+    // log('DROPZONE: Leave ')
+    // this = dropzone
+    this.classList.remove('over')
+
+}
+
+function drop() {
+    // log('DROPZONE: dropped ')
+    this.classList.remove('over')
+}
